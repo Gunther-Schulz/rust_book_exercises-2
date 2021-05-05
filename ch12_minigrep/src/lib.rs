5cd4cf5a6a4f5e466329a7827f7b4c0f409fd94c
Gunther Schulz <dev@guntherschulzu.de>
2021-05-05 15:50:16 +0200

    add improvements from chapter 13.3

--- ch12_minigrep/src/lib.rs
+++ ch12_minigrep/src/lib.rs
@@ -25,25 +25,66 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
+
         let mut o: Vec<String> = vec![];
         let mut p: Vec<String> = vec![];
 
+        // let foo: Vec<String> = vec!["a".to_string(), "b".to_string()];
+        // let foox: Vec<String> = args.collect();
+
+        // let active_classes: Vec<&String> = foo.iter().filter(|x| x == &&"a".to_string()).collect();
+        // let active_classes: Vec<&String> = foo.iter().filter(|x| x != &&"a".to_string()).collect();
+        // let non_active_classes: Vec<_> = foo.iter().filter(|x| x.status != Status::Active).collect();
+
+        // // fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+        // //     shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+        // // }
+
+        // let args: Vec<String> = env::args().collect();
+        // let p: Vec<&String> = foox
+        //     .iter()
+        //     .filter(|&item| item.contains("--") || item.contains("-"))
+        //     .collect();
+        // let o: Vec<&String> = foox
+        //     .iter()
+        //     .filter(|&item| !item.contains("--") || !item.contains("-"))
+        //     .collect();
+        // let o: Vec<String> = args.filter(|item| item.contains("--") || item.contains("-")).collect();
+
         for item in args {
             if item.contains("--") || item.contains("-") {
-                p.push(item.clone());
+                p.push(item);
             } else {
-                o.push(item.clone())
+                o.push(item)
             }
         }
 
-        if o.len() < 2 {
-            return Err("not enough arguments");
-        }
-        let query = o[0].clone();
-        let filename = o[1].clone();
+        let p_iter = p.into_iter();
+        let mut o_iter = o.into_iter();
 
-        let case_sensitive = is_case_sensitive(&p);
+        let mut case_sensitive = true;
+        if !env::var("CASE_INSENSITIVE").is_err() {
+            case_sensitive = false;
+        }
+        for w in p_iter {
+            match &w as &str {
+                "-i" => case_sensitive = false,
+                _ => (),
+            }
+        }
+
+        let query = match o_iter.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match o_iter.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
         println!("{:?}", case_sensitive);
 
         Ok(Config {
@@ -55,40 +96,20 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
+
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
-fn is_case_sensitive(args: &[String]) -> bool {
-    if env::var("CASE_INSENSITIVE").is_err() {
-        return false;
-    }
-    for item in args {
-        if item == "-i" {
-            return false;
-        }
-    }
-    true
-}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -119,9 +140,9 @@ Trust me.";
             search_case_insensitive(query, contents)
         );
     }
-    #[test]
-    fn t_case_sensitive() {
-        let args: Vec<String> = vec!["-i".to_string(), "foo".to_string(), "faa".to_string()];
-        assert!(!is_case_sensitive(&args))
-    }
+    // #[test]
+    // fn t_case_sensitive() {
+    //     let args: Vec<String> = vec!["-i".to_string(), "foo".to_string(), "faa".to_string()];
+    //     assert!(!is_case_sensitive(&args))
+    // }
 }
